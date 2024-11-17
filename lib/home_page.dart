0dca169f400c1620f7090e6409fb59c846ea2c6e
M.Younus_M <mohammadyounusafghan16@gmail.com>
2024-11-17 05:56:08 -0800

    first commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main(List<String> args) {
   runApp(home());
@@ -23,16 +23,16 @@ class _homeState extends State<home> {
         body: 
         Column(
           children: [
-        items(context, 'one', "iphone 4"),
-        items(context, 'one', "iphone 5"),
-        items(context, 'one', "iphone 6"),
-        items(context, 'one', "iphone 10"),
-        items(context, 'one', "iphone 11"),
-        items(context, 'one', "iphone X"),
-        items(context, 'one', "iphone 12"),
-        items(context, 'one', "iphone 13"),
-        items(context, 'one', "iphone 15"),
-        items(context, 'one', "iphone 16"),
+        items(context, "one", "iphone 4"),
+        items(context, "one", "iphone 5"),
+        items(context, "one", "iphone 6"),
+        items(context, "one", "iphone 10"),
+        items(context, "one", "iphone 11"),
+        items(context, "one", "iphone X"),
+        items(context, "one", "iphone 12"),
+        items(context, "one", "iphone 13"),
+        items(context, "one", "iphone 15"),
+        items(context, "one", "iphone 16"),
           ],
         )
       ),
@@ -43,18 +43,19 @@ class _homeState extends State<home> {
 
 
 items(BuildContext context, address, name){
-  ListView(
-children: [
-  ListTile(
-    leading: Icon(Icons.apple),
-    title: Text("$name"),
+ return Expanded(
+   child: ListView(
+   children: [
+    ListTile(
+      leading: Icon(Icons.apple),
+      title: Text("$name"),
 
-  onTap: () {
-    Navigator.pushNamed(context, "$address");
-  },
-
-  ),
-
-],
-);
+    onTap: () {
+      Navigator.pushNamed(context, "$address");
+    },
+    ),
+   
+   ],
+   ),
+ );
 }
--- lib/items.dart
+++ lib/items.dart
@@ -4,20 +4,11 @@ class one extends StatelessWidget {
   const one({super.key});
   @override
   Widget build(BuildContext context) {
-    var detail ="""ایفون 4
-رسما معرفی شد و پاسخ تمام سوال های چند هفته گذشته که پیش اومده بود، معلوم شد که آیفون گیزمودو و ویتنامی ها حقیقی بوده و اپل این بار در مخفی نگهداشتن جدیدترین محصول خود تا روز معرفی گوشی رسمی موفق نبوده است.
-""";
-    var img = 'iphone.jpg';
+
     return MaterialApp(
-      home: abc(context, "Behnam Bani", detail, img)
-    );
-  }
-}
-
-
-abc(BuildContext context, title, detail, img) {
-  return Scaffold(
-    appBar: AppBar(
+      home:
+      Scaffold(
+      appBar: AppBar(
       leading: IconButton(
         onPressed: () {
           Navigator.pop(context);
@@ -25,7 +16,7 @@ abc(BuildContext context, title, detail, img) {
         icon: const Icon(Icons.keyboard_backspace),
       ),
       title: Text(
-        "$title",
+        "آیفون",
         style: const TextStyle(
           fontSize: 40,
         ),
@@ -36,21 +27,29 @@ abc(BuildContext context, title, detail, img) {
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Image(
-          image: AssetImage('assets/$img'),
+        Expanded(
+          child: Image(
+            image: AssetImage('assets/iphone.jpg'),
+          ),
         ), 
 
-        Container(
-          child: Text(
-            """$detail""",
-            style: const TextStyle(
-              fontSize: 25,
-              color: Color.fromRGBO(12, 32, 44, 1),
-            ), 
+        Expanded(
+          child: Container(
+            child: Text(
+             """ایفون 4
+        رسما معرفی شد و پاسخ تمام سوال های چند هفته گذشته که پیش اومده بود، معلوم شد که آیفون گیزمودو و ویتنامی ها حقیقی بوده و اپل این بار در مخفی نگهداشتن جدیدترین محصول خود تا روز معرفی گوشی رسمی موفق نبوده است.""",
+              style: const TextStyle(
+                fontSize: 25,
+                color: Color.fromRGBO(12, 32, 44, 1),
+              ), 
+            ),
           ),
         ),
 
       ],
     ),
-  );
+  ),
+    );
+  }
 }
+
